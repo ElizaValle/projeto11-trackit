4b5e3adcc2a9705450764651711253cc67ed48aa
ElizaValle <elizabetevalle7@gmail.com>
2023-03-14 14:30:09 -0300

    layout rota cadastro

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -2,7 +2,7 @@ import logo from "../../assets/logo.png"
 import { ScreenContainer, Logo, FormContainer } from "./styled";
 import { Link } from "react-router-dom";
 
-export default function HomePage() {
+export default function RegistrationPage() {
     return (
         <ScreenContainer>
             <Logo src={logo} />
@@ -11,11 +11,15 @@ export default function HomePage() {
                 <input id="email" name="email" type="email" placeholder="email" required />
                 <label htmlFor="senha"></label>
                 <input id="senha" name="senha" type="password" placeholder="senha" required />
+                <label htmlFor="nome"></label>
+                <input id="nome" name="nome" type="text" placeholder="nome" required />
+                <label htmlFor="foto"></label>
+                <input id="foto" name="foto" type="link" placeholder="foto" required />
 
-                <button type="submit">Entrar</button>
+                <button type="submit">Cadastrar</button>
 
                 <Link>
-                    <p>Não tem uma conta? Cadastre-se!</p>
+                    <p>Já tem uma conta? Faça login!</p>
                 </Link>
             </FormContainer> 
         </ScreenContainer> 
---
+++ src/pages/RegistrationPage/styled.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const ScreenContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Logo = styled.img`
+    margin-top: 68px;
+` 
+
+export const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 32px 0;
+    input {
+        width: 303px;
+        height: 45px;
+        margin-bottom: 6px;
+        padding-left: 11px;
+        border: 1px solid #d5d5d5;
+        border-radius: 5px;
+        font-family: 'Lexend Deca', sans-serif;
+        font-size: 19px;
+        color: #dbdbdb;
+    }
+    button {
+        width: 318px;
+        height: 45px;
+        background-color: #52B6FF;
+        border-radius: 5px;
+        border: 1px solid #D5D5D5;
+        font-family: 'Lexend Deca', sans-serif;
+        font-size: 20px;
+        color: #FFFFFF;
+    }
+    a {
+        margin-top: 25px;
+        font-family: 'Lexend Deca', sans-serif;
+        font-size: 13px;
+        color: #52B6FF;
+    }
+`
